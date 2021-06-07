9b279057473dab80abc78614728ee562a84b2a6f
Pratap <pratap.1804@gmail.com>
2021-06-07 20:26:22 +0530

    Added Random method to calculate the Attendamce of the Employee

---
+++ EmpWageUC1.java
@@ -0,0 +1,11 @@
+public class EmpWageUC1 {
+	public static void main(String args[]) {
+	System.out.println("Welcome to Employee Wage Problem");
+	double res = Math.random();
+	if(res > 0.5) {
+		System.out.println("Employee is Present");
+	} else{
+		System.out.println("Employee is Absent");
+			}
+	}
+}
